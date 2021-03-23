f593bc85b8782cb64b77d649b98602e6395c3aad
Karen Roh <karenroh@hotmail.com>
2021-03-23 09:44:52 -0400

    api call for 5 day daily forecast working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,7 @@
 const searchBtn = document.getElementById("search-button");
 const weatherContainerEl = document.getElementById("weather-container");
 // Step 1: Get current weather API for any city
-function getApi() {
-  let city = document.getElementById("city-name").value;
+function getApi(city) {
   let apiKey = "0ae0fe7cc5a483cdff07255ca0a1a19f";
   let requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   fetch(requestUrl)
@@ -10,7 +9,7 @@ function getApi() {
       return res.json();
     })
     .then((data) => {
-      displayWeather(data);
+      displayCurrentWeather(data);
       console.log(data);
     })
     .catch((err) => {
@@ -19,14 +18,47 @@ function getApi() {
 }
 
 // Step 2: Display data on page
-function displayWeather(weatherInfo) {
-  console.log(weatherInfo.name);
+//need to display icon..
+function displayCurrentWeather(weatherInfo) {
   let cityName = weatherInfo.name;
-  let cityInfo = document.createElement("div");
-  cityInfo.innerHTML = `
-  <h1>${cityName}</h1>`;
-  weatherContainerEl.append(cityInfo);
+  let cityNameEl = document.getElementById("searched-city");
+  let currentDate = new Date(weatherInfo.dt * 1000).toLocaleDateString();
+  let currentDateEl = document.getElementById("current-date");
+  let icon = weatherInfo.weather[0]["icon"];
+  let iconUrl = `http://openweathermap.org/img/wn/${icon}.png`;
+  let iconEl = document.getElementById("icon-rep");
+  iconEl.setAttribute("src", iconUrl);
+
+  for (let i = 0; i < cityName.length; i++) {
+    if (cityName === undefined) {
+      return;
+    } else {
+      cityNameEl.innerText = cityName;
+      currentDateEl.innerText = currentDate;
+      iconEl.innerText = iconEl;
+    }
+  }
+  getApi2(city);
 }
+// Get next 5 day daily forecast API
+function getApi2(city) {
+  let apiKey = "0ae0fe7cc5a483cdff07255ca0a1a19f";
+  let requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=Atlanta&units=imperial&cnt=5&appid=${apiKey}`;
+  fetch(requestUrl)
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      //   displayFutureWeather(data);
+      console.log(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// working api: 'api.openweathermap.org/data/2.5/forecast?q=Atlanta&appid=0ae0fe7cc5a483cdff07255ca0a1a19f'
+
 // GIVEN a weather dashboard with form inputs
 // WHEN I search for a city
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -38,4 +70,7 @@ function displayWeather(weatherInfo) {
 // THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, and the humidity
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
-searchBtn.addEventListener("click", getApi);
+searchBtn.addEventListener("click", function () {
+  let city = document.getElementById("city-name").value;
+  getApi(city);
+});
